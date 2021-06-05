9a5a34eaf1f2ac3cbface717a09703a75e515fa4
Anisha Desktop <anisharanjan55@gmail.com>
2021-06-04 21:40:08 -0400

    Solution w/ comment.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,23 @@
 package org.example;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Summer 2021 Assignment 15 Solution
+ *  Copyright 2021 Anisha Ranjan
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is the password? ");
+        String password = in.nextLine();
+
+        if(password.equals("abc$123")) {
+            System.out.print("Welcome!");
+        } else {
+            System.out.print("I don't know you.");
+        }
     }
 }
